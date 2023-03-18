2a2436c74f895f39aae205ffd43038bee9951e70
Marcellan-Echo <trish.olori15@outlook.com>
2023-03-18 01:57:39 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+  * This is my first comment in C Programming
+  * return value is always 0
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
