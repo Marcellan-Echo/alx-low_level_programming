1587f277040ce0b7c69a0fcad07e7d0eff60ea3a
Marcellan-Echo <trish.olori15@outlook.com>
2023-03-18 02:24:22 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 # include <stdio.h>
 /**
-  * This is my first comment in C Programming
-  * return value is always 0
-  */
+ *main - Entry point of the program
+ *Description: This function prints a message to the console using the `puts`
+ *function. The function returns 0 to indicate success.
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
