76c014959b2aedd8fe5f27f6b2ed0f8f8813d603
Marcellan-Echo <trish.olori15@outlook.com>
2023-03-21 20:14:44 +0000

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ * main - main function
+ * Return: return 0 when successful
+ */
+
+
+void print_alphabet(void)
+
+{
+
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+
+}
