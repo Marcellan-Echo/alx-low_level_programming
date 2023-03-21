f96b5de0025ec4fddb256560cb4e99a728b990c2
Marcellan-Echo <trish.olori15@outlook.com>
2023-03-21 20:30:08 +0000

    Alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints alphabets 10x
+ * Return: return 0
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	int i;
+	int j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+
+
+}
